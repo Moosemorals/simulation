52f8bb55aa7101f6e2e59e0a5d9d42b499968572
Osric Wilkinson <aosricw@ukho.gov.uk>
2026-03-27 11:00:53 +0000

    feat: enhance simulation architecture with ECS components and systems, including position and velocity management

--- src/uk.osric.sim.simulation/Ecs/Components/Position.cs
+++ src/uk.osric.sim.simulation/Ecs/Components/Position.cs
@@ -1,6 +1,6 @@
 // SPDX-FileCopyrightText: Copyright (c) 2026 Osric Wilkinson <osric@fluffypeople.com>
 // SPDX-License-Identifier: ISC
 
-namespace uk.osric.sim.simulation.Ecs;
+namespace uk.osric.sim.simulation.Ecs.Components;
 
-public readonly record struct EntityId(int Value);
+public readonly record struct Position(float X, float Y);
--- src/uk.osric.sim.simulation/Ecs/Components/Sheep.cs
+++ src/uk.osric.sim.simulation/Ecs/Components/Sheep.cs
@@ -1,6 +1,6 @@
 // SPDX-FileCopyrightText: Copyright (c) 2026 Osric Wilkinson <osric@fluffypeople.com>
 // SPDX-License-Identifier: ISC
 
-namespace uk.osric.sim.simulation.Ecs;
+namespace uk.osric.sim.simulation.Ecs.Components;
 
-public readonly record struct EntityId(int Value);
+internal readonly struct Sheep { }
